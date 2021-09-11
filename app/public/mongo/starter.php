64ed640b20ae4621d3293ceaedf508d32e36bf25
Eray Aydın <eray.aydin@ozguryazilim.com.tr>
2021-09-11 07:23:00 +0300

    Add mongo example import to the doc root.

---
+++ app/public/mongo/starter.php
@@ -0,0 +1,28 @@
+<?php
+
+$manager = new MongoDB\Driver\Manager('mongodb://mongo/');
+
+$collections = ['units', 'civilizations', 'structures'];
+
+array_map(
+    static fn ($line): string => printf("%s%s", $line, PHP_SAPI === 'cli' ? PHP_EOL : "<br>"),
+    array_map(
+        static fn (
+            MongoDB\Driver\WriteResult $result,
+            string $collection
+        ): string => sprintf("Inserted %d docs for the %s", $result->getInsertedCount(), $collection),
+        array_map(
+            static function (string $file) use ($manager) {
+                $bulk = new MongoDB\Driver\BulkWrite;
+
+                array_map(static function ($item) use ($bulk) {
+                    $bulk->insert($item);
+                }, json_decode(file_get_contents(__DIR__ . "/{$file}.json"), true, 5, JSON_THROW_ON_ERROR)->{$file});
+
+                return $manager->executeBulkWrite("aoe2.{$file}", $bulk);
+            },
+            $collections
+        ),
+        $collections
+    )
+);
